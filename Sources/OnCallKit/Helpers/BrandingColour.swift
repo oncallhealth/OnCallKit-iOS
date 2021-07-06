fa1fca727e118c9e69be5494e6c5b7a1b0306229
Domenic Bianchi <domenic@oncallhealth.com>
2021-07-06 13:02:48 -0400

    Revert "Save"
    This reverts commit df5401c97c69ea093f83a8fa991bd35651432b20.

--- Sources/OnCallKit/Helpers/BrandingColour.swift
+++ Sources/OnCallKit/Helpers/BrandingColour.swift
@@ -10,6 +10,6 @@ import Foundation
 // MARK: BrandingColour
 
 public struct BrandingColour {
-    public let light: String
-    public let dark: String
+    let light: String
+    let dark: String
 }
